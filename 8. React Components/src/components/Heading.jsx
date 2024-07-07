cb2e480fde6cc292449afde13ba21b6fb3687c9a
AniketR9 <rajeshaniket13@gmail.com>
2024-07-07 11:48:10 +0530

    Lec 8 done.

---
+++ 8. React Components/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;
+// We won't add the parenthesis above bcos it will immediately return our function
+// the moment it gets imported.
